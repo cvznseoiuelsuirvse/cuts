9d7c1489d0d25fdd5d72da6afe7c90521ab84721
cvznseoiuelsuirvse <fedorlavlinsky@gmail.com>
2026-03-24 01:43:04 -0400

    =DD

--- genprotos.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/env bash
-py scripts/wayland-codegen.py protocols/xdg-shell.xml wayland/types/xdg-shell
-py scripts/wayland-codegen.py protocols/linux-dmabuf-v1.xml wayland/types/linux-dmabuf-v1
-py scripts/wayland-codegen.py protocols/wayland.xml wayland/types/wayland
-
--- send.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# ninja -C build && scp -P 22000 ./build/main user@localhost:~
-rsync -av -e "ssh -p 22000" --exclude='.git' --filter=':- .gitignore' ../$(basename $(pwd)) user@localhost:~
